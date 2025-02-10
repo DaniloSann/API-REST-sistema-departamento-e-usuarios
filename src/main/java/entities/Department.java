16da9b545693ff587f74a9a3f56787ca9c936ba1
dsantana <danilosan262020@gmail.com>
2025-02-10 11:25:14 -0300

    Mapeamento objeto relacional das duas entidades

--- src/main/java/entities/Department.java
+++ src/main/java/entities/Department.java
@@ -1,6 +1,14 @@
 package entities;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,10 +1,20 @@
 package entities;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
     public User() {
     }
