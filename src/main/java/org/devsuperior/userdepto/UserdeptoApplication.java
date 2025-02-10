684b957f1060f6abd15a22944ec1e706f3bfe7f8
Santana <danilo.santana@capgemini.com>
2025-02-10 10:47:14 -0300

    Initial commit

---
+++ src/main/java/org/devsuperior/userdepto/UserdeptoApplication.java
@@ -0,0 +1,13 @@
+package org.devsuperior.userdepto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserdeptoApplication.class, args);
+    }
+
+}
