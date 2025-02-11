cb05b9ea4b49644086584632937161737f47c79f
dsantana <danilosan262020@gmail.com>
2025-02-11 10:00:57 -0300

    endpoint para buscar todos os usuários (Correção)

--- src/main/java/userdept/UserRepository.java
+++ src/main/java/userdept/UserRepository.java
@@ -3,5 +3,8 @@ package userdept;
 import entities.User;
 import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepositoryExtension<User, Long> {
+    List<User> findAll();
 }
