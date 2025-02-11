8c2e1af2617c7199bc6873d88246dfde98672272
dsantana <danilosan262020@gmail.com>
2025-02-11 10:16:25 -0300

    endpoint para salvar um usuario (correção)

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -2,10 +2,7 @@ package controllers;
 
 import entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import userdept.UserRepository;
 
 import java.util.List;
@@ -27,5 +24,10 @@ public class UserController {
     public User findById(@PathVariable Long id) {
         User result = repository.findById(id).get();
         return result;
+    }
+
+    @PostMapping
+    public User insert(@RequestBody User user) {
+        return repository.save(user);
     }
 }
