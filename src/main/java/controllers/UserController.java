2c36db7e41af8e85acbe9bdc63e695dda1da92b9
dsantana <danilosan262020@gmail.com>
2025-02-11 10:08:54 -0300

    endpoint de busca de usuario por ID

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -3,6 +3,7 @@ package controllers;
 import entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import userdept.UserRepository;
@@ -19,6 +20,12 @@ public class UserController {
     @GetMapping
     public List<User> findAll() {
         List<User> result = repository.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "{/id}")
+    public User findById(@PathVariable Long id) {
+        User result = repository.findById(id).get();
         return result;
     }
 }
--- src/main/java/userdept/UserRepository.java
+++ src/main/java/userdept/UserRepository.java
@@ -1,10 +1,11 @@
 package userdept;
 
 import entities.User;
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepositoryExtension<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
 }
